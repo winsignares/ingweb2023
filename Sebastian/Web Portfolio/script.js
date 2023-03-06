9042b717d79b8a46429c0a45e2e41d3517b6b783
Daniel_Lerma <SOPORTE@SALA1-08-BAQ.unilibre.edu.co>
2023-03-06 09:44:15 -0500

    sebastian

---
+++ Sebastian/Web Portfolio/script.js
@@ -0,0 +1,12 @@
+import Typed from 'typed.js';
+// Herramineta Header
+
+let MenuBtn = document.getElementById('MenuBtn');
+
+MenuBtn.addEventListener('click', function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+this.classList.toggle('fa-xmark') 
+
+})
+
+// Efecto JS
